7501011dcf3eeba3950f4a147ef5330b78cefcda
Julien Viet <julien@julienviet.com>
2013-11-30 17:45:19 +0100

    Base roller command that does nothing

---
+++ src/crash/commands/roller/roller.java
@@ -0,0 +1,12 @@
+import org.crsh.cli.Usage
+import org.crsh.cli.Command
+
+class roller
+{
+  @Usage("Roller base command that does nothing")
+  @Command
+  void main() {
+  	def factory = org.apache.roller.weblogger.business.WebloggerFactory.getWeblogger();
+  	out << "The roller factory ${factory}";
+ }
+}
